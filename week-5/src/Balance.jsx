ed922519a27bf8112fbaa0c49234548481efd0c2
AyushSri5 <156398511+AyushSri5@users.noreply.github.com>
2024-10-15 23:28:50 +0530

    week-5 assignment

--- week-5/src/Balance.jsx
+++ week-5/src/Balance.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import { clusterApiUrl, Connection, LAMPORTS_PER_SOL, PublicKey } from '@solana/web3.js';
+
 
 const Balance = () => {
-  return (
-    <div>
-      
-    </div>
+  const address = useRef(null);
+  const [balance, setBalance] = useState(null);
+  console.log("Address", address);
+
+  const getBalance = async() => {
+      const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+
+      let wallet = new PublicKey(address.current.value);
+      console.log(
+        `${(await connection.getBalance(wallet)) / LAMPORTS_PER_SOL} SOL`,
+      );
+      setBalance((await connection.getBalance(wallet)) / LAMPORTS_PER_SOL);
+  }
+  
+return (
+  <div>
+      <h1>Solana Balance:</h1>
+      <label>Enter the address</label>
+      <input type="text" ref={address} />
+      <button onClick={getBalance}>Show Balance</button>
+      <p>{balance}</p>
+  </div>
   )
 }
 
